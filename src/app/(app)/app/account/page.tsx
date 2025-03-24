cf6030376de206feb727ee59b06765e21c0b401f
Amith B V <amithrao0606@gmail.com>
2025-03-24 13:17:44 +0530

    Done with Account page content + styling.

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
-    <div>
-      <p className="text-2xl font-bold underline">Account page</p>
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-white">Account</h1>
+
+      <p className="text-white mt-2">
+        Logged in with email :{" "}
+        <span className="font-bold">amithrao0606@gmail.com</span>
+      </p>
     </div>
   );
 }
