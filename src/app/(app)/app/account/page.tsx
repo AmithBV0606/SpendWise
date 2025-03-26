e374837bbdeffcf071ae97ff07628ea001d88459
Amith B V <amithrao0606@gmail.com>
2025-03-26 21:19:21 +0530

    Displaying user email in account's route.

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -3,19 +3,20 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   // Authentication Check : User should only be able to view the account only when he/she is signedIn
-  const { isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated, getUser } = getKindeServerSession();
 
   if (!(await isAuthenticated())) {
     return redirect("/api/auth/login");
   }
+
+  const user = await getUser();
 
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold text-white">Account</h1>
 
       <p className="text-white mt-2">
-        Logged in with email :{" "}
-        <span className="font-bold">amithrao0606@gmail.com</span>
+        Logged in with email : <span className="font-bold">{user.email}</span>
       </p>
     </div>
   );
