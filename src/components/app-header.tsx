1666c884ee3ae36b44b9f04f55b85ccbc5b3a714
Amith B V <amithrao0606@gmail.com>
2025-03-26 16:44:04 +0530

    Implemented the Logout feature.

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -13,7 +14,7 @@ export default function AppHeader() {
   const pathname = usePathname();
 
   return (
-    <header className="flex justify-between items-center border-b border-white/25 py-4">
+    <header className="flex items-center border-b border-white/25 py-4">
       <Link href={"/app/dashboard"}>
         <Image
           src={
@@ -26,14 +27,14 @@ export default function AppHeader() {
         />
       </Link>
 
-      <nav>
+      <nav className="ml-auto">
         <ul className="flex gap-2 text-xs">
           {routes.map((route) => (
             <li key={route.path}>
               <Link
                 href={route.path}
                 className={`px-2 py-2 hover:text-white transition text-white/100 rounded-sm cursor-pointer ${
-                  route.path === pathname ? "bg-white/20" : ""
+                  route.path === pathname ? "bg-black/10" : ""
                 }`}
               >
                 {route.label}
@@ -42,6 +43,10 @@ export default function AppHeader() {
           ))}
         </ul>
       </nav>
+
+      <LogoutLink className="text-white/70 text-[12px] ml-[10px]">
+        Logout
+      </LogoutLink>
     </header>
   );
 }
